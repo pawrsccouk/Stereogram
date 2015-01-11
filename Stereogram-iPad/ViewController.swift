5b21fb9485ae660c948d470fda9c9719cba19e71
Patrick A Wallace <pawrsccouk@googlemail.com>
2015-01-11 15:52:02 +0000

    Initial Commit

---
+++ Stereogram-iPad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stereogram-iPad
+//
+//  Created by Patrick Wallace on 11/01/2015.
+//  Copyright (c) 2015 Patrick Wallace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
